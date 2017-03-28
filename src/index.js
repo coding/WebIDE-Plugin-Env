2a375df532d4ccff17ab15b9e9e895e0a8f0f90c
郑鑫祺 <candy.zhengxq@gmail.com>
2017-03-28 18:04:56 +0800

    注册app时使用sdk的appRegistry方法，去除无用代码

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 require('./base-theme/index.styl');
 const app = require('./app').default;
-const reducer = require('./reducer').default;
 const Manager = require('./manager').default;
+const appRegistry = require('codingSDK/utils').appRegistry;
 
-module.exports = {
+appRegistry({
   app,
-  reducer,
   Manager,
-}
-
-window.codingPackageJsonp(module.exports)
+});
