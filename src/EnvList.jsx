422cb9951b4547908e571570cbe5218e8902e849
veedrin <veedrin@qq.com>
2018-04-16 11:22:27 +0800

    add envlist icon

--- src/EnvList.jsx
+++ src/EnvList.jsx
@@ -5,6 +5,7 @@ import * as EnvActions from './actions';
 import cx from 'classnames';
 import global from './global';
 import ServerInfo from './ServerInfo';
+import getSvg from '../static';
 
 const Modal = global.sdk.Modal;
 const i18n = global.i18n;
@@ -97,8 +98,8 @@ class EnvList extends Component {
   handleDelete = async (name, e) => {
     e.preventDefault()
     var confirmed = await Modal.showModal('Confirm', {
-      header: i18n`list.handleDelete.header`,      
-      message: i18n`list.handleDelete.message${{name}}`,      
+      header: i18n`list.handleDelete.header`,
+      message: i18n`list.handleDelete.message${{name}}`,
       okText: i18n`list.handleDelete.okText`
     })
     Modal.dismissModal()
@@ -109,9 +110,9 @@ class EnvList extends Component {
   handleSwitch = async (name, e) => {
     e.preventDefault()
     var confirmed = await Modal.showModal('Confirm', {
-      header: i18n`list.handleSwitch.header`,      
-      message: i18n`list.handleSwitch.message${{name}}`,      
-      okText: i18n`list.handleSwitch.okText`,      
+      header: i18n`list.handleSwitch.header`,
+      message: i18n`list.handleSwitch.message${{name}}`,
+      okText: i18n`list.handleSwitch.okText`,
     })
     Modal.dismissModal()
     if (confirmed) {
@@ -140,10 +141,8 @@ const EnvItem = ({node, isCurrent, handleSave, handleReset, handleDelete, handle
   return (
     <div className={cx('env-item', { current: isCurrent })}>
       <div className="env-item-heading">
-        <i className="fa fa-desktop" />
-        {node.isGlobal ? <span className="env-label env-public">{i18n`list.global`}</span> : ''}
-        {isShared ? <span className="env-label env-shared">{i18n`list.shared`}</span> : ''}
-        {node.name}
+        {node.isGlobal ? getSvg(node.displayName) : getSvg('share')}
+        {node.displayName}
       </div>
       <div className="env-item-body">
         {node.owner ? (
@@ -163,7 +162,7 @@ const EnvItem = ({node, isCurrent, handleSave, handleReset, handleDelete, handle
           </button> */}
           <button className="btn btn-primary btn-sm" onClick={handleReset.bind(null, node.name)}>
             <i className="fa fa-undo" />
-            {i18n`list.reset`}         
+            {i18n`list.reset`}
           </button>
         </div>
       ) : (
@@ -195,7 +194,7 @@ const mapStateToProps = (state) => {
       envList = [], currentEnv = null, operating, operatingMessage
     }
   } = state
-  return ({ 
+  return ({
     envList,
     currentEnv,
     operating,
