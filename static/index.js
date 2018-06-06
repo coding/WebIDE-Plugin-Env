2228833b36f4664b788f56a4e05fbe7aac285fe4
veedrin <veedrin@qq.com>
2018-06-06 16:32:46 +0800

    env rename and fix bug

--- static/index.js
+++ static/index.js
@@ -1,22 +1,40 @@
 import React from 'react';
+
 import ubuntu from '!raw-loader!./ubuntu.svg';
-import node from '!raw-loader!./nodejs.svg';
+import nodejs from '!raw-loader!./nodejs.svg';
 import java from '!raw-loader!./java.svg';
 import php from '!raw-loader!./php.svg';
-import net from '!raw-loader!./dotnet.svg';
+import dotnet from '!raw-loader!./dotnet.svg';
 import elixir from '!raw-loader!./elixir.svg';
 import machinelearning from '!raw-loader!./machinelearning.svg';
 import jekyll from '!raw-loader!./jekyll.svg';
 import ruby from '!raw-loader!./ruby.svg';
 import hexo from '!raw-loader!./hexo.svg';
+import go from '!raw-loader!./go.svg';
+import c from '!raw-loader!./c.svg';
 import share from '!raw-loader!./share.svg';
 
-const svgs = { ubuntu, node, java, php, net, elixir, machinelearning, jekyll, ruby, hexo, share };
+const svgs = {
+	'nodejs': nodejs,
+	'jekyll': jekyll,
+	'hexo': hexo,
+	'php-mysql': php,
+	'rvm': ruby,
+	'rbenv': ruby,
+	'java': java,
+	'dotnet': dotnet,
+	'machine-learning': machinelearning,
+	'go': go,
+	'c-cpp': c,
+}
 
 const match = (label) => {
+	if (label.indexOf('php-python-java') !== -1) {
+		return ubuntu;
+	}
 	for (let key in svgs) {
 		if (svgs.hasOwnProperty(key)) {
-			if (label.indexOf(key) > -1) {
+			if (label.indexOf(key) !== -1) {
 				return svgs[key];
 			}
 		}
